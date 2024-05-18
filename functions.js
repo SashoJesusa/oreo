936acba3021aca72899d2d10a92745eec5bd8ec2
Алекс Видев <alx.videv@gmail.com>
2024-05-18 16:20:40 +0300

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,13 +1,22 @@
 function displayWelcomeMessage() {
-    // Get the element where the welcome message will be displayed
     var welcomeElement = document.getElementById('welcomeMessage');
-
-    // Define the welcome message
     var message = 'Welcome to our law firm\'s website. We are committed to providing you the best legal services.';
-
-    // Display the welcome message
     welcomeElement.textContent = message;
 }
-
-// Call the function when the page is loaded
 window.onload = displayWelcomeMessage;
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (firstName === '' || lastName === '' || email === '' || message === '') {
+        alert('All fields are required.');
+        event.preventDefault();
+    } else if (email.indexOf('@') === -1) {
+        alert('Please enter a valid email address.');
+        event.preventDefault();
+    }
+});
