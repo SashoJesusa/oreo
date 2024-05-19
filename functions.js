af1183077538f104ff8bc3b00e246712df8326a9
Александър Миланов <alex0110.milanov@gmail.com>
2024-05-19 15:48:18 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -32,3 +32,16 @@ function toggleList() {
         button.textContent = 'Achievements';
     }
 }
+
+  var acc = document.getElementsByClassName("accordion");
+        for (var i = 0; i < acc.length; i++){
+        acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+        panel.style.display = "none";
+        } else {
+        panel.style.display = "block";
+       }
+    });
+  }
